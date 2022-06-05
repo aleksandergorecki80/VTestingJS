afeb1cd8cbdbc2f358c290c97774615f2ec8b7a9
FoxCote <aleksandergorecki80@gmail.com>
2022-06-05 08:33:51 +0200

    Testing ASYNC - AWAIT

--- Section_06-Asynchronus code/async/async-example.test.js
+++ Section_06-Asynchronus code/async/async-example.test.js
@@ -20,3 +20,21 @@ it('should generate a token in promisse', () => {
     const result = generateTokenPromise(testUserEmail);
     expect(result).resolves.toBeDefined();
 })
+
+it('generated token should be a string', () => {
+    const testUserEmail = 'jankowalski@gmai.com';
+    const result = generateTokenPromise(testUserEmail);
+    expect(result).resolves.toBeTypeOf('string');
+})
+
+it('generated token should be a string ASYNC - AWAIT', async () => {
+    const testUserEmail = 'jankowalski@gmai.com';
+    const result = await generateTokenPromise(testUserEmail);
+    expect(result).toBeTypeOf('string');
+})
+
+it('should generate a token value - (RETURN ASSERTION)', () => {
+    const testUserEmail = 'test@test.com';
+    const result = generateTokenPromise(testUserEmail);
+    return expect(result).resolves.toBeDefined();
+  });
